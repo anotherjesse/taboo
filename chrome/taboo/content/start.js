b19227654cfa081db7868a681239bb9a31c6f7b6
Ian Fischer <itfische@gmail.com>
2007-07-20 02:13:39 -0700

    Added basic code to allow search on typing, and made the creation of the taboos a function.

--- chrome/taboo/content/start.js
+++ chrome/taboo/content/start.js
@@ -2,33 +2,38 @@ const CC = Components.classes;
 const CI = Components.interfaces;
 const SVC = CC['@oy/taboo;1'].getService(CI.oyITaboo);
 
-var ul = document.createElement('ul');
-
-var enum = SVC.get(null);
-while (enum.hasMoreElements()) {
-  var tab = enum.getNext();
-  tab.QueryInterface(CI.oyITabooInfo);
-
-  // this should be a custom xbl:
-  (function (tab){ 
-    var box = document.createElement('li');
-    box.setAttribute('title', tab.title);
-    box.innerHTML = '<span class="delete" title="delete taboo"></span><span class="title"><nobr>' +
-      tab.title + '</nobr></span><span class="url" title="'+ tab.url +'">' +
-      tab.url + '</span><img class="preview" src="' + tab.imageURL + '" />';
-
-    box.onclick = function(event) {
-      if (event.originalTarget.className == 'delete') {
-        SVC.delete(tab.url);
-        box.parentNode.removeChild(box);
-      }
-      else {
-        SVC.open(tab.url, whereToOpenLink(event));
-      }
-    }
-
-    ul.appendChild(box);
-  })(tab);
+var searchText = null;
+function loadTaboos() {
+	var ul = document.createElement('ul');
+	ul.setAttribute('id', 'taboos');
+	
+	var enum = SVC.get(searchText);
+	while (enum.hasMoreElements()) {
+	  var tab = enum.getNext();
+	  tab.QueryInterface(CI.oyITabooInfo);
+	
+	  // this should be a custom xbl:
+	  (function (tab){ 
+	    var box = document.createElement('li');
+	    box.setAttribute('title', tab.title);
+	    box.innerHTML = '<span class="delete" title="delete taboo"></span><span class="title"><nobr>' +
+	      tab.title + '</nobr></span><span class="url" title="'+ tab.url +'">' +
+	      tab.url + '</span><img class="preview" src="' + tab.imageURL + '" />';
+	
+	    box.onclick = function(event) {
+	      if (event.originalTarget.className == 'delete') {
+	        SVC.delete(tab.url);
+	        box.parentNode.removeChild(box);
+	      }
+	      else {
+	        SVC.open(tab.url, whereToOpenLink(event));
+	      }
+	    }
+	
+	    ul.appendChild(box);
+	  })(tab);
+	}
+	
+	document.body.appendChild(ul);
 }
 
-document.body.appendChild(ul);
