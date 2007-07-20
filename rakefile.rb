6ac784453d52dccd9dcc91b8ea4f23d3eb45dde9
Jesse Andrews <anotherjesse@gmail.com>
2007-07-20 15:13:06 -0700

    task to build xpi

--- rakefile.rb
+++ rakefile.rb
@@ -4,3 +4,9 @@ file 'components/oyITaboo.xpt' => 'components/oyITaboo.idl' do
   puts "Generating oyITaboo.xpt. (requires flock dev environment)"
   `xpidl -m typelib -w -v -I $o/dist/idl -o components/oyITaboo components/oyITaboo.idl`
 end
+
+
+task :xpi do
+  rm_f 'taboo.xpi'
+  `find chrome components install.rdf defaults | xargs zip taboo.xpi`
+end 
