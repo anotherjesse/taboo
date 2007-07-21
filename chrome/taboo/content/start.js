e7de1f6d0ca1b2db6ef14d247be3a84f037d2698
Ian Fischer <itfische@gmail.com>
2007-07-20 23:33:56 -0700

    In the process of refactoring loadTaboos(), commiting to merge with Jesse.

--- chrome/taboo/content/start.js
+++ chrome/taboo/content/start.js
@@ -5,36 +5,38 @@ const SVC = CC['@oy/taboo;1'].getService(CI.oyITaboo);
 var ul = document.createElement('ul');
 var searchText = null;
 
-function loadTaboos() {
-	var ul = document.createElement('ul');
-	ul.setAttribute('id', 'taboos');
-	
-	var enum = SVC.get(searchText);
+function loadTaboos(display) {
+  display.container.setAttribute('id', 'taboos');
+
+  var enum = SVC.get(searchText);
   while (enum.hasMoreElements()) {
     var tab = enum.getNext();
     tab.QueryInterface(CI.oyITabooInfo);
 
-    // this should be a custom xbl:
-    (function (tab){ 
-      var box = document.createElement('li');
-      box.innerHTML = '<div title="'+tab.title+'"><span class="delete" title="delete taboo"></span><span class="title"><nobr>' +
-        tab.title + '</nobr></span><span class="url" title="'+ tab.url +'">' +
-        tab.url + '</span><img class="preview" src="' + tab.imageURL + '" /></div>';
-
-      box.onclick = function(event) {
-        if (event.originalTarget.className == 'delete') {
-          SVC.delete(tab.url);
-          box.parentNode.removeChild(box);
-        }
-        else {
-          SVC.open(tab.url, whereToOpenLink(event));
-        }
-      }
-
-      ul.appendChild(box);
-    })(tab);
+    displayFunc(tab);
   }
 	
-	document.body.appendChild(ul);
+  document.body.appendChild(display.container);
 }
 
+
+var normalStartPage = {
+	container: document.createElement('ul'),
+	display: function(tab) {
+		var box = document.createElement('li');
+		box.innerHTML = '<div title="'+tab.title+'"><span class="delete" title="delete taboo"></span><span class="title"><nobr>' +
+		  tab.title + '</nobr></span><span class="url" title="'+ tab.url +'">' +
+		  tab.url + '</span><img class="preview" src="' + tab.imageURL + '" /></div>';
+		
+		box.onclick = function(event) {
+		  if (event.originalTarget.className == 'delete') {
+		    SVC.delete(tab.url);
+		    box.parentNode.removeChild(box);
+		  }
+		  else {
+		    SVC.open(tab.url, whereToOpenLink(event));
+		  }
+		}
+		this.container.appendChild(box);
+	}
+}
