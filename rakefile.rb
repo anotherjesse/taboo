2a6afd101dcef31c12dbef2f65d419ece4ad9011
Johan Sundström <jhs@jhsmacbook.local>
2008-05-02 01:11:28 +0200

    Space optimisation: don't add empty directories to the xpi. :-)

--- rakefile.rb
+++ rakefile.rb
@@ -8,5 +8,6 @@ end
 
 task :xpi do
   rm_f 'taboo.xpi'
-  `find chrome chrome.manifest components install.rdf | xargs zip taboo.xpi`
+  `find chrome chrome.manifest components install.rdf -type f \
+   | xargs zip taboo.xpi`
 end 
