2bcb1cf8ab2de0ab709e746d90f1cedd0ca19c2f
Jesse Andrews <anotherjesse@gmail.com>
2008-06-22 14:56:42 -0700

    fix the firefox 2 issue

--- chrome/content/vendor/jquery.hue.js
+++ chrome/content/vendor/jquery.hue.js
@@ -22,35 +22,50 @@ jQuery.fn.hue = function(options) {
   var special_hide = function() {
     hide();
     timeout = setTimeout(function() {
-			   if (OVER_ITEM) {
-			     SHOWING = true;
-			     outerbox.show();
-			   }
-			 }, 800);
+                           if (OVER_ITEM) {
+                             SHOWING = true;
+                             outerbox.show();
+                           }
+                         }, 500);
   };
 
+  var mouseX, mouseY;
+
   body
-    .mousemove(special_hide)
+    .mousemove(function(event) {
+                 // Firefox 2 reports a mousemove when a new div
+                 // gets added underneath the mouse
+
+                 if (event.clientX == mouseX &&
+                     event.clientY == mouseY) {
+                   return;
+                 }
+
+                 mouseX = event.clientX;
+                 mouseY = event.clientY;
+
+                 special_hide();
+               })
     .click(hide)
     .keypress(hide)
     .bind('hue.over',
-	  function(event, node, callback) {
-	    OVER_ITEM = true;
-	    box.empty();
-	    outerbox[0].onclick = null;
-	    box.append(node);
-	    outerbox[0].onclick = callback;
-	  })
+          function(event, node, callback) {
+            OVER_ITEM = true;
+            box.empty();
+            outerbox[0].onclick = null;
+            box.append(node);
+            outerbox[0].onclick = callback;
+          })
     .bind('hue.out',
-	  function() {
-	    // deal with if we are showing or not
-	    OVER_ITEM = SHOWING || false;
-	  })
+          function() {
+            // deal with if we are showing or not
+            OVER_ITEM = SHOWING || false;
+          })
     .bind('hue.hide',
-	  function() {
-	    SHOWING = false;
-	    outerbox.hide();
-	  });
+          function() {
+            SHOWING = false;
+            outerbox.hide();
+          });
 
   hide();
 };
