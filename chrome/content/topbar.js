c625028d402726b0e6efb16bf5ac923d88a931af
Erwan Loisant <eloisant@gmail.com>
2008-07-12 15:23:32 -0700

    flock topbar

---
+++ chrome/content/topbar.js
@@ -0,0 +1,28 @@
+
+const SVC = Components.classes['@oy/taboo;1'].getService(Components.interfaces.oyITaboo);
+
+var tabooTopbar = {
+
+  add: function(tab) {
+    var slidey = document.createElement("slideyitem");
+    var image = document.createElement("taboo");
+    image.setAttribute("src", tab.thumbURL);
+    slidey.appendChild(image);
+    slidey.onclick = function(event) {
+      SVC.open(tab.url, whereToOpenLink(event));
+    };
+    this.slideybox.appendChild(slidey);
+  },
+
+  onLoad: function() {
+    this.slideybox = document.getElementById("taboo-slideybox");
+    var taboos = SVC.get(null, false);
+
+    while (taboos.hasMoreElements()) {
+      var tab = taboos.getNext();
+      tab.QueryInterface(Components.interfaces.oyITabooInfo);
+      this.add(tab);
+    }
+  }
+
+}
