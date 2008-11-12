7d62dad1294309256d3337c0dd7fe42ef45330c0
joshua <joshua@Macintosh-9.local>
2008-11-11 20:16:22 -0800

    Added defaults. (DOH!)

---
+++ defaults/preferences/tabooprefs.js
@@ -0,0 +1,4 @@
+pref("extensions.taboo.enabled", true );
+pref("extensions.taboo.lastversion", "firstrun" );
+pref("extensions.taboo.firstRunURL", "http://taboo.spandexfox.com/welcome/" );
+pref("extensions.taboo.upgradeURL", "http://taboo.spandexfox.com/upgrade/" );
